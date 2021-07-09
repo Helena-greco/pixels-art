1bd3de27dd45c3c221e9638e8e1203c601e9f61d
Helena <nenafgreco@hotmail.com>
2021-07-09 15:21:16 -0300

    Requisito 9 incompleto

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
-window.onload = function load() {
-  document.querySelector('.black').classList.add('selected');
-};
+window.onload = document.querySelector('.black').classList.add('selected');
 
 const colorPalette = document.querySelectorAll('.color');
-const pixels = document.querySelectorAll('.pixel');
 const black = document.querySelector('.black');
 const red = document.querySelector('.red');
 const yellow = document.querySelector('.yellow');
 const blue = document.querySelector('.blue');
+const pixels = document.querySelectorAll('.pixel');
+const color = ['black', 'blue', 'red', 'yellow'];
 
 function colorSelected(event) {
   for (let index = 0; index < colorPalette.length; index += 1) {
@@ -20,3 +19,12 @@ black.addEventListener('click', colorSelected);
 red.addEventListener('click', colorSelected);
 yellow.addEventListener('click', colorSelected);
 blue.addEventListener('click', colorSelected);
+
+function clean() {
+  for (let index = 0; index < pixels.length; index += 1) {
+    pixels[index].style.backgroundColor = 'white';
+  }
+}
+
+const clearButton = document.getElementById('clear-board');
+clearButton.addEventListener('click', clean);
