fc9fe8306fb9d3041849bba3d475ca5f6a564e7b
Helena <nenafgreco@hotmail.com>
2021-07-11 13:55:22 -0300

    Requisito 8 FINALMENTE, e Requisito 9

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const red = document.querySelector('.red');
 const yellow = document.querySelector('.yellow');
 const blue = document.querySelector('.blue');
 const pixels = document.querySelectorAll('.pixel');
-const color = ['black', 'blue', 'red', 'yellow'];
+const palette = document.querySelector('#color-palette');
 
 function colorSelected(event) {
   for (let index = 0; index < colorPalette.length; index += 1) {
@@ -20,9 +20,32 @@ red.addEventListener('click', colorSelected);
 yellow.addEventListener('click', colorSelected);
 blue.addEventListener('click', colorSelected);
 
+function selectedColor(event) {
+  const color = event.target;
+  for (let i = 0; i < palette.children.length; i += 1) {
+    palette.children[i].classList.remove('selected');
+  }
+  color.classList.add('selected');
+}
+palette.addEventListener('click', selectedColor);
+
+const table = document.querySelector('#pixel-board');
+
+function paint(event) {
+  for (let i = 0; i < palette.children.length; i += 1) {
+    if (palette.children[i].classList.contains('selected')) {
+      const color = window.getComputedStyle(palette.children[i], null).getPropertyValue('background-color');
+      const block = event.target;
+      block.style.backgroundColor = color;
+    }
+  }
+}
+
+table.addEventListener('click', paint);
+
 function clean() {
-  for (let index = 0; index < pixels.length; index += 1) {
-    pixels[index].style.backgroundColor = 'white';
+  for (let i2 = 0; i2 < pixels.length; i2 += 1) {
+    pixels[i2].style.backgroundColor = 'white';
   }
 }
 
